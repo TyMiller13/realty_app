9aae2afe6b7bf98426170ccf51bd6000da4a9e96
unknown <tylermiller1321@outlook.com>
2023-02-28 16:21:27 -0600

    working on email verification

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,5 @@
+import * as config from "../config.js";
+
 export const welcome = (req, res) => {
     res.json({
         data: "hello from nodejs api from routes... "
@@ -11,8 +13,42 @@ export const preRegister = async (req, res) => {
      // when user clicks on the link then registration is complete
     try {
         console.log(req.body);
+            config.AWSSES.sendEmail(
+            {
+                Source: config.EMAIL_FROM,
+                Destination: {
+                    ToAddresses: ["tmilli21@gmail.com"],
+                },
+                Message: {
+                    Body: {
+                        Html: {
+                            Charset: "UTF-8",
+                            Data: `
+                            <html>
+                                <h1> Welcome to Homey! </h1>
+                            </html>
+                            `,
+                        },
+                    },
+                    Subject: {
+                        Charset: "UTF-8",
+                        Data: "Welcome to Homey!"
+                    }
+            
+                }
+            }, 
+            (err, data) => {
+                if(err) {
+                    console.log(err);
+                    return res.json({ ok: false});
+                } else {
+                    console.log(data);
+                    return res.json({ ok: true});
+                }
+            }
+        );
     } catch (err) {
         console.log(err);
         return res.json({error: 'Something went wrong.. Please try again.'});
     }
-}
+};
