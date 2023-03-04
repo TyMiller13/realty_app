7cd9ec436da456790db4bdf935da2c21fa69009a
unknown <tylermiller1321@outlook.com>
2023-03-04 00:02:52 -0600

    adding refresh token request, refactored code, get current user logged in request

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -6,6 +6,16 @@ import User from '../models/user.js'
 import { nanoid } from "nanoid";
 import validator from "email-validator"
 
+//function that creates JWT and sends user and token response -- for repeated use
+const tokenAndUserResponse = (req, res, user) => {
+    const token = jwt.sign({ _id: user._id }, config.JWT_SECRET, {expiresIn: "1h",});
+    const refreshToken = jwt.sign({ _id: user._id }, config.JWT_SECRET, {expiresIn: "7d",});
+
+    user.password = undefined;
+    user.resetCode = undefined;
+
+    return res.json({ token, refreshToken, user});  
+}
 
 export const welcome = (req, res) => {
     res.json({
@@ -35,7 +45,6 @@ export const preRegister = async (req, res) => {
         }
 
         const user = await User.findOne({ email });
-        
         if(user){
             return res.json({ error: "Email is already registered"})
         }
@@ -65,20 +74,21 @@ export const register = async (req, res) => {
     try {
         // console.log(req.body);
         const { email, password } = jwt.verify(req.body.token, config.JWT_SECRET);
+
+        const existingUser = await User.findOne({ email });
+        if(existingUser){
+            return res.json({ error: "Email is already registered"})
+        }
+
         const hashedPassword = await hashPassword(password);
         const user = await new User({
             username: nanoid(8),
             email, 
             password: hashedPassword,
         }).save();
+        
+        tokenAndUserResponse(req, res, user);
 
-        const token = jwt.sign({ _id: user._id }, config.JWT_SECRET, {expiresIn: "1h",});
-        const refreshToken = jwt.sign({ _id: user._id }, config.JWT_SECRET, {expiresIn: "7d",});
-
-        user.password = undefined;
-        user.resetCode = undefined;
-
-        return res.json({ token, refreshToken, user});
     } catch (err) {
         console.log(err);
         return res.json({error: 'Something went wrong..Please try again'}); // i.e. user already exists
@@ -100,18 +110,9 @@ export const login = async (req, res) => {
           error: "Wrong password",
         });
       }
-      //  create json web token
-      const token = jwt.sign({ _id: user._id }, config.JWT_SECRET, {expiresIn: "1h",});
-      const refreshToken = jwt.sign({ _id: user._id }, config.JWT_SECRET, {expiresIn: "7d",});
-      //send user and token response (dont include PW)
-      user.password = undefined;
-      user.resetCode = undefined;
-  
-      return res.json({
-        user,
-        token,
-        refreshToken,
-      });
+
+      tokenAndUserResponse(req, res, user);
+
     } catch (err) {
       console.log(err);
       res.json({ error: "Something went wrong. Try again." });
@@ -151,24 +152,40 @@ export const login = async (req, res) => {
 
   export const accessAccount = async (req, res) => {
     try {
-        const { resetCode } = jwt.verify(req.body.resetCode, config.JWT_SECRET);
+        const { resetCode } = jwt.verify(req.body.token, config.JWT_SECRET);
 
-        const user = await User.findOneAndUpdate({ resetCode }, { resetCode: "" })
-              
-      const token = jwt.sign({ _id: user._id }, config.JWT_SECRET, {expiresIn: "1h",});
-      const refreshToken = jwt.sign({ _id: user._id }, config.JWT_SECRET, {expiresIn: "7d",});
+        const user = await User.findOneAndUpdate({ resetCode }, { resetCode: "" });
       
-      user.password = undefined;
-      user.resetCode = undefined;
-  
-      return res.json({
-        user,
-        token,
-        refreshToken,
-      });
+        tokenAndUserResponse(req, res, user);
+      
     } catch (err) {
         console.log(err);
         res.json({ error: "Something went wrong. Try again." });       
     }
   };
   
+  export const refreshToken = async (req, res) => {
+    try {
+       const {_id} = jwt.verify(req.headers.refresh_token, config.JWT_SECRET);
+       const user = await User.findById(_id);
+
+       tokenAndUserResponse(req, res, user);
+
+    } catch (err) {
+        console.log(err);
+        return res.status(403).json({ error: "Failed to refresh token" }); // forbidden route if token is not refreshed          
+    }
+
+  };
+
+  export const currentUser = async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id);
+        user.password = undefined;
+        user.resetCode = undefined;
+        res.json(user);
+    } catch (err) {
+        console.log(err);
+        return res.status(403).json({ error: "Unauthorized" });
+    }
+  }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -11,6 +11,8 @@ router.post('/register', auth.register); // create the register endpoint
 router.post('/login', auth.login); // login 
 router.post('/forgot-password', auth.forgotPassword);// forgot password
 router.post('/access-account', auth.accessAccount); // account access
+router.get('/refresh-token', auth.refreshToken)
+router.get('/current-user', requireLogin, auth.currentUser)
 
 
 
