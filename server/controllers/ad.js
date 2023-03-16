b47f2efac9f24031f318a4664e7e66b2345c6557
unknown <tylermiller1321@outlook.com>
2023-03-16 13:06:40 -0500

    fixed bug with image functionality

--- server/controllers/ad.js
+++ server/controllers/ad.js
@@ -1,17 +1,16 @@
 import * as config from "../config.js";
 import { nanoid } from "nanoid";
 
-
-
 export const uploadImage = async (req, res) => {
     try {
-        const {image} = req.body;
-
-        //prepare image 
-        const base64Image = new Buffer.from(image.replace(/^data:image\/\w+;base64,/, ""), "base64");
+        // console.log(req.body);
+        const { image } = req.body;
+        const base64Image = new Buffer.from(
+            image.replace(/^data:image\/\w+;base64,/, ""),
+            "base64"
+        );
         const type = image.split(';')[0].split('/')[1];
-
-        //image params
+        //create image params
         const params = {
             Bucket: "homey-realty-bucket",
             Key: `${nanoid()}.${type}`,
@@ -25,23 +24,24 @@ export const uploadImage = async (req, res) => {
                 console.log(err);
                 res.sendStatus(400);
             } else {
-                // console.log(data);
+                console.log(data);
                 res.send(data);
             }
-        })
+        });
     } catch (err) {
-        console.log(err);
-        res.json({error: "Upload failed.. try again."});
+        console.log(err)
+        res.json({error: "Upload failed, try again."})
     }
-};
+}
+
+
 
 export const removeImage = async (req, res) => {
     try {
-        const {Key, Bucket} = req.body;
+        const { Key, Bucket } = req.body;
 
-        //modify s3 bucket
-        config.AWS_S3.deleteObject({Bucket, Key}, (err, data) =>{
-            if(err){
+        config.AWS_S3.deleteObject({Bucket, Key}, (err, data) => {
+            if(err) {
                 console.log(err);
                 res.sendStatus(400);
             } else {
