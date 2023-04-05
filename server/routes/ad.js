a7fe7a4bbbfbf4e6aaebed569150e963bc462dc6
unknown <tylermiller1321@outlook.com>
2023-04-05 16:51:15 -0500

    created new route and queried ads in server for sell and rent ads

--- server/routes/ad.js
+++ server/routes/ad.js
@@ -8,5 +8,5 @@ import { requireLogin } from "../middleware/auth.js";
 router.post('/upload-image', requireLogin, ad.uploadImage);
 router.post('/remove-image', requireLogin, ad.removeImage);
 router.post('/ad', requireLogin, ad.create);
-
+router.get('/ads', ad.ads )
 export default router;
