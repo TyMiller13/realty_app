fe7e0920aa8135df935717cc685c505bfc524b02
unknown <tylermiller1321@outlook.com>
2023-04-14 16:20:19 -0500

    formatted price for cards, add forsale or rent ribbon to card

--- client/src/components/cards/AdCard.jsx
+++ client/src/components/cards/AdCard.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import {IoBedOutline} from 'react-icons/io5';
 import {TbBath} from 'react-icons/tb';
 import {BiArea} from 'react-icons/bi';
+import {Badge} from 'antd';
 
 
 
 
 export default function AdCard({ad}) {
+    function formatNumber(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+    }
+
   return (
     <div className='col-lg-4 p-4 gx-4 gy-4'>
+        <Badge.Ribbon text={`${ad?.type} for ${ad?.action}`} color={`${ad?.action === "Sell" ? "blue" : "green"}`}>
         <div className="card hoverable shadow">
             <img 
                 src={ad?.photos?.[0].Location} 
@@ -16,9 +22,10 @@ export default function AdCard({ad}) {
                 style={{ height:"250px", objectFit: "cover"}} 
             />
             <div className="card-body">
-                <div className="d-flex justify-content-between">
-                    <h3>{ad?.price}</h3>
-                </div>
+                
+                    <h3>${formatNumber(ad?.price)}</h3>
+                    <p className='card-text'>`{ad?.address}`</p>
+                
                 <p className='card-text d-flex justify-content-between'>
                     {ad?.bedrooms ? (<span> <IoBedOutline/> {ad?.bedrooms} </span>) : '' }
                     {ad?.bathrooms ? (<span> <TbBath/> {ad?.bathrooms} </span>) : '' }
@@ -26,6 +33,7 @@ export default function AdCard({ad}) {
                 </p>
             </div>
         </div>
+        </Badge.Ribbon>
     </div>
   )
 }
